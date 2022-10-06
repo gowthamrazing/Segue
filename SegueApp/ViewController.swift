61ee42ea4f521bbade24a62ccf4536498c20299b
gowthamrazing <gowthamrazing@gmail.com>
2022-10-07 00:12:40 +0800

    Segue moving data from one screen to next screen

--- SegueApp/ViewController.swift
+++ SegueApp/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var userName = ""
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mLabel1: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func nextClicked(_ sender: Any) {
+        userName = nameTextField.text!
+        
+        performSegue(withIdentifier: "toSecondVC", sender: nil)
+        
+       // nameTextField.text =
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSecondVC"{
+            let destinationVC = segue.destination as! SecondViewController
+            
+            destinationVC.myName = userName
+        }
+    }
+    
 }
 
